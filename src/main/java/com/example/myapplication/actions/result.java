a02ad5a89a1222b4a17820e15222fbe78b8e3adc
Kanstantsin Khushenski <46225922+KonstDev@users.noreply.github.com>
2022-05-08 13:22:42 +0300

    fixed bigInteger issues

--- src/main/java/com/example/myapplication/actions/result.java
+++ src/main/java/com/example/myapplication/actions/result.java
@@ -1,10 +1,14 @@
 package com.example.myapplication.actions;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.*;
+
 import de.congrace.exp4j.Calculable;
 import de.congrace.exp4j.ExpressionBuilder;
-
 //TODO: fix big integers
 
 public class result {
+    BigDecimal I;
     private static String check_0(double ev)
     {
         Integer u = 0;
@@ -22,8 +26,8 @@ public class result {
         Calculable calc = null;
         try {
             calc = new ExpressionBuilder(ev).build();
-            double result = calc.calculate();
-            ev = check_0(result);
+            BigDecimal result = new BigDecimal(calc.calculate());
+            ev = result.toString();
             //Log.d(TAG, "onClick: ");
         } catch (Exception e) {
             ev = "Math Error";
